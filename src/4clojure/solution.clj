f46e87fff2836b60105b60c84a2d544dc01240c9
Clifton McIntosh <cliff.mcintosh@gmail.com>
2016-09-21 19:28:21 -0500

    Through problem 38, maximum value

--- src/4clojure/solution.clj
+++ src/4clojure/solution.clj
@@ -47,3 +47,38 @@
     (if (empty? remaining)
       reversed
       (recur (rest remaining) (conj reversed (first remaining))))))
+
+
+;; http://www.4clojure.com/problem/27
+;; Palindrome Detector
+;; Write a function which returns true if the given sequence is a palindrome.
+(fn [original] (= (seq original) (reverse (seq original))))
+
+
+;; http://www.4clojure.com/problem/26
+;; Fibonacci Sequence
+;; Write a function which returns the first X fibonacci numbers.
+(fn [x]
+  (take x
+        ((fn fib [a b]
+           (cons a (lazy-seq (fib b (+ a b)))))
+         1 1)))
+
+
+;; http://www.4clojure.com/problem/38
+;; Maximum value
+;; Write a function which takes a variable number of parameters and returns
+;; the maximum value.
+(fn max-value
+  [& more]
+  (cond (empty? more)
+        more
+
+        (empty? (rest more))
+        (first more)
+
+        (> (first more) (second more))
+        (recur ( cons (first more) (rest (rest more))))
+
+        :else
+        (recur (rest more))))

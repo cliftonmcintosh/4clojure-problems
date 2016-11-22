a782ae3ac04b663f45bc7f898a943feeaf169239
Clifton McIntosh <cliff.mcintosh@gmail.com>
2016-11-22 07:04:17 -0600

    through problem 85: Power Set

--- src/4clojure/solution.clj
+++ src/4clojure/solution.clj
@@ -767,6 +767,69 @@
               (range 1 n))))))
 
 
+;; http://www.4clojure.com/problem/86
+;; Happy numbers
+;; Happy numbers are positive integers that follow a particular formula: take
+;; each individual digit, square it, and then sum the squares to get a new
+;; number. Repeat with the new number and eventually, you might get to a number
+;; whose squared sum is 1. This is a happy number. An unhappy number (or sad
+;; number) is one that loops endlessly. Write a function that determines if a
+;; number is happy or not.
+(fn is-happy' [n]
+  (letfn [(square [x] (* x x))]
+    (cond (= 1 n) true
+          (= 4 n) false
+          :else (recur (->> n
+                            str
+                            seq
+                            (map #(-> %
+                                      str
+                                      (Integer/parseInt)
+                                      square))
+                            (apply +))))))
+
+
+;; http://www.4clojure.com/problem/78
+;; Reimplement Trampoline
+;; Reimplement the function described in "Intro to Trampoline".
+(fn trampoline' [f & args]
+  (let [outcome (apply f args)]
+    (if (fn? outcome)
+      (trampoline' outcome)
+      outcome)))
+
+
+;; http://www.4clojure.com/problem/115
+;; The Balance of N
+;; A balanced number is one whose component digits have the same sum on the left
+;; and right halves of the number. Write a function which accepts an integer n,
+;; and returns true if n is balanced.
+(fn balance-of-n [n]
+  (let [s (map (fn [x]
+                 (->> x
+                      str
+                      Integer/parseInt))
+               (str n))
+        half (quot (count s) 2)]
+    (= (reduce + (take half s))
+       (reduce + (take-last half s)))))
+
+
+;; http://www.4clojure.com/problem/85
+;; Power Set
+;; Write a function which generates the power set of a given set. The power set
+;; of a set x is the set of all subsets of x, including the empty set and x
+;; itself.
+(fn power-set [superset]
+  (reduce (fn [base-set item]
+            (into base-set (map
+                            (fn [s] (conj s item))
+                            base-set)))
+          #{#{}}
+          superset))
+
+
+
 ;; http://www.4clojure.com/problem/121
 ;; Universal Computation Engine
 ;; Given a mathematical formula in prefix notation, return a function that

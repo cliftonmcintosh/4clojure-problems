f7e68c208940b2427233a9dd784f5f8dee4e342a
Cliff McIntosh <cmcintosh@opinionlab.com>
2016-12-05 20:33:20 -0600

    Explorations for word chains.

--- src/4clojure/scratch.clj
+++ src/4clojure/scratch.clj
@@ -1,1 +1,15 @@
 (ns scratch)
+
+
+(defn word-chains [ws]
+  (let [freqs (map frequencies ws)
+        finder (fn [x y]
+                 (as-> (merge-with - x y) $
+                   (group-by val $)
+                   (count (get $ 1))
+                   (or (= 1 $) (= 2 $))))]
+    (every? #(contains? % true)
+            (map set (for [f freqs]
+                       (map #(finder f %) (remove #(= f %) freqs)))))))
+
+(word-chains #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})

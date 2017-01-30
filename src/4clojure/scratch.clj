7583447be9e17785ccfaecfb8e36cf4d073697fc
Cliff McIntosh <cmcintosh@opinionlab.com>
2017-01-30 16:12:18 -0600

    WIP tricky card games

--- src/4clojure/scratch.clj
+++ src/4clojure/scratch.clj
@@ -1,1 +1,24 @@
 (ns scratch)
+
+
+(defn tricky [cards trump]
+  (let [lead (:suit (first cards))
+        find-best-suit (fn [suited]
+                         (if (and trump (seq (trump suited)))
+                           (trump suited)
+                           (lead suited)))]
+    (->> cards
+         (group-by :suit)
+         find-best-suit
+         (sort-by :rank)
+         last)))
+
+(def cards [{:suit :spade :rank 2}
+            {:suit :spade :rank 3}
+            {:suit :club :rank 10}])
+
+(tricky cards :heart)
+
+
+(= {:suit :club :rank 10} ((tricky :club) [{:suit :spade :rank 2}
+                                       {:suit :club :rank 10}]))

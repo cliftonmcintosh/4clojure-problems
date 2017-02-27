3c8d3bbd8dc32d777c1448db54a7121a60203fb2
Clifton McIntosh <cliff.mcintosh@gmail.com>
2017-02-27 07:01:38 -0600

    Scratch for game of life

--- src/4clojure/scratch.clj
+++ src/4clojure/scratch.clj
@@ -1,1 +1,27 @@
 (ns scratch)
+
+
+(def example ["      "
+              " ##   "
+              " ##   "
+              "   ## "
+              "   ## "
+              "      "])
+
+
+(defn game-of-life [rs]
+  (let [height (count rs)
+        indexed-chars
+        (apply merge
+               (for [r (range height)]
+                 (->> r
+                      (nth rs)
+                      (map-indexed (fn [idx itm] [[r idx] itm]))
+                      (reduce
+                       (fn [accum itm]
+                         (merge accum {(first itm) (second itm)}))
+                       {}))))]
+    (filter #(= \# (val %)) indexed-chars)))
+
+
+(game-of-life example)

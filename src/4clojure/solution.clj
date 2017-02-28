b37ee4eef040cf8ec785930d3c06dae59a540e91
Cliff McIntosh <cmcintosh@opinionlab.com>
2017-02-27 19:48:29 -0600

    Solved problem 94: Game of Life

--- src/4clojure/solution.clj
+++ src/4clojure/solution.clj
@@ -1328,3 +1328,52 @@
               false
               (recur (apply conj visited neighbors)
                      (remove (apply conj #{} neighbors) unvisited)))))))
+
+
+;; https://www.4clojure.com/problem/94
+;; Game of Life
+;; The game of life is a cellular automaton devised by mathematician John
+;; Conway.
+;; The 'board' consists of both live (#) and dead ( ) cells. Each cell interacts
+;; with its eight neighbours (horizontal, vertical, diagonal), and its next
+;; state is dependent on the following rules:
+;; 1) Any live cell with fewer than two live neighbours dies, as if caused by
+;; under-population.
+;; 2) Any live cell with two or three live neighbours lives on to the next
+;; generation.
+;; 3) Any live cell with more than three live neighbours dies, as if by
+;; overcrowding.
+;; 4) Any dead cell with exactly three live neighbours becomes a live cell, as
+;; if by reproduction.
+;; Write a function that accepts a board, and returns a board representing the
+;; next generation of cells.
+(defn game-of-life [rs]
+  (let [row-length (count (first rs))
+        neighbors-fn
+        (fn [r c]
+          (let [p-row (dec r)
+                n-row (inc r)
+                p-column (dec c)
+                n-column (inc c)]
+            (count (filter #(= \# %)
+                           [(get-in rs [p-row p-column])
+                            (get-in rs [p-row c])
+                            (get-in rs [p-row n-column])
+                            (get-in rs [r p-column])
+                            (get-in rs [r n-column])
+                            (get-in rs [n-row p-column])
+                            (get-in rs [n-row c])
+                            (get-in rs [n-row n-column])]))))]
+    (map #(apply str %)
+         (partition row-length
+                    (for [r (range (count rs))
+                          c (range row-length)]
+                      (let [cell (get-in rs [r c])
+                            live-neighbors (neighbors-fn r c)
+                            is-live? (= \# cell)]
+                        (cond (and is-live? (<= 2 live-neighbors 3))
+                              \#
+                              (and (not is-live?) (= 3 live-neighbors))
+                              \#
+                              :else
+                              \space)))))))

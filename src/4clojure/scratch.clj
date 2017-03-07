fac901773891ff352f6876e956678bbd9bab3685
Clifton McIntosh <cliff.mcintosh@gmail.com>
2017-03-06 21:22:51 -0600

    palindromic numbers clunky solution that works (quickly) locally.

--- src/4clojure/scratch.clj
+++ src/4clojure/scratch.clj
@@ -1,55 +1,48 @@
 (ns scratch)
 
-(defn next-palindrome [n]
-  (if (> 10 n)
-    (lazy-seq (cons (inc n) (next-palindrome (inc n))))
-    (let [num-str (str n)
-          num-len (count num-str)
-          midway (int (/ num-len 2))
-          first-half (->> num-str
-                          (take midway)
-                          reverse
-                          (apply str))
-          last-half (->> num-str
-                         (drop midway)
-                         (apply str))
-          head (if (<= (Long/valueOf first-half)
-                       (Long/valueOf last-half))
-                 (inc (Long/valueOf (apply str (take
-                                                (if (even? num-len)
-                                                  midway
-                                                  (inc midway)) num-str))))
-                 (Long/valueOf (apply str (take (if (even? num-len)
-                                                  midway
-                                                  (inc midway))  num-str))))
-          current (Long/valueOf (str head (->> head
-                                               str
-                                               (take midway)
-                                               reverse
-                                               (apply str))))]
-      (lazy-seq (cons current (next-palindrome current))))))
+(defn pal-numbers [x]
+  (letfn [(palindrome [n]
+            (let [num-str (str n)
+                  num-len (count num-str)
+                  midway (int (/ num-len 2))
+                  first-half (->> num-str
+                                  (take midway)
+                                  reverse
+                                  (apply str))
+                  last-half (->> num-str
+                                 (drop midway)
+                                 (apply str))
+                  head (if (<= (Long/valueOf first-half)
+                               (Long/valueOf last-half))
+                         (inc (Long/valueOf (apply str (take
+                                                        (if (even? num-len)
+                                                          midway
+                                                          (inc midway)) num-str))))
+                         (Long/valueOf (apply str (take (if (even? num-len)
+                                                          midway
+                                                          (inc midway))  num-str))))
+                  current (Long/valueOf (str head (->> head
+                                                       str
+                                                       (take midway)
+                                                       reverse
+                                                       (apply str))))]
+              (lazy-seq (cons current (palindrome current)))))]
+    (if (= (str x) (clojure.string/reverse (str x)))
+      (lazy-seq (cons x (pal-numbers (inc x))))
+      (palindrome x))))
 
-(take 2 (next-palindrome 0))
+(take 26 (pal-numbers 0))
 
-(take 16 (next-palindrome 162))
+(take 16 (pal-numbers 162))
 
-(take 6 (next-palindrome 1234550000))
+(take 6 (pal-numbers 1234550000))
 
-(first (next-palindrome (* 111111111 111111111)))
+(first (pal-numbers (* 111111111 111111111)))
 
-(apply < (take 6666 (next-palindrome 9999999)))
+(= (first (pal-numbers (* 111111111 111111111)))
+   (* 111111111 111111111))
 
+(apply < (take 6666 (pal-numbers 9999999)))
 
-;; def next_palindrome(n):
-;; """
-;; Given a non-negative integer n, return the first integer strictly
-;; greater than n whose decimal representation is palindromic.
-;;
-;; """
-;;   s = str(n + 1)
-;;   l = len(s)
-;;   if s[:l//2][::-1] < s[(l+1)//2:]:
-;;     head = str(int(s[:(l+1)//2])+1)
-;;   else:
-;;     head = s[:(l+1)//2]
-;;   return int(head + head[:l//2][::-1])
+(= (nth (pal-numbers 0) 10101)
+   9102019)

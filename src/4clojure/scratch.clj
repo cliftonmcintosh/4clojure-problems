ffd5b9789c744c21761dd810841f1d3464557d71
Clifton McIntosh <cliff.mcintosh@gmail.com>
2017-03-08 21:12:22 -0600

    Palindromic numbers, not string-based, but still times out on 4clojure

--- src/4clojure/scratch.clj
+++ src/4clojure/scratch.clj
@@ -1,48 +1,58 @@
 (ns scratch)
 
-(defn pal-numbers [x]
-  (letfn [(palindrome [n]
-            (let [num-str (str n)
-                  num-len (count num-str)
-                  midway (int (/ num-len 2))
-                  first-half (->> num-str
-                                  (take midway)
-                                  reverse
-                                  (apply str))
-                  last-half (->> num-str
-                                 (drop midway)
-                                 (apply str))
-                  head (if (<= (Long/valueOf first-half)
-                               (Long/valueOf last-half))
-                         (inc (Long/valueOf (apply str (take
-                                                        (if (even? num-len)
-                                                          midway
-                                                          (inc midway)) num-str))))
-                         (Long/valueOf (apply str (take (if (even? num-len)
-                                                          midway
-                                                          (inc midway))  num-str))))
-                  current (Long/valueOf (str head (->> head
-                                                       str
-                                                       (take midway)
-                                                       reverse
-                                                       (apply str))))]
+
+(defn pal-nums [x]
+  (letfn [(pal-coll [n]
+            (loop [y n
+                   accum []]
+              (if (< y 10)
+                (cons y accum)
+                (recur (long (/ y 10)) (cons (rem y 10) accum)))))
+          (val-for-part [num-coll]
+            (apply + (map-indexed
+                      (fn [index item]
+                        (long (* (java.lang.Math/pow 10 index) item)))
+                      num-coll)))
+          (palindrome [n]
+            (let [data (pal-coll n)
+                  num-len (count data)
+                  midway (long (/ num-len 2))
+                  first-half (take midway data)
+                  last-half (drop midway data)
+                  val-for-head (val-for-part (if (even? num-len)
+                                               (reverse first-half)
+                                               (reverse (take (inc midway) data))))
+                  head (if (<= (val-for-part first-half)
+                               (val-for-part (reverse last-half)))
+                         (inc val-for-head)
+                         val-for-head)
+                  head-coll (pal-coll head)
+                  current (val-for-part (concat head-coll (->> head-coll
+                                                               (take midway)
+                                                               reverse)))]
               (lazy-seq (cons current (palindrome current)))))]
-    (if (= (str x) (clojure.string/reverse (str x)))
-      (lazy-seq (cons x (pal-numbers (inc x))))
+    (if (= (pal-coll x) (reverse (pal-coll x)))
+      (lazy-seq (cons x (pal-nums (inc x))))
       (palindrome x))))
 
-(take 26 (pal-numbers 0))
 
-(take 16 (pal-numbers 162))
+(take 26 (pal-nums 0))
 
-(take 6 (pal-numbers 1234550000))
+(take 16 (pal-nums 162))
 
-(first (pal-numbers (* 111111111 111111111)))
+(take 6 (pal-nums 1234550000))
 
-(= (first (pal-numbers (* 111111111 111111111)))
+
+(= (first (pal-nums (* 111111111 111111111)))
    (* 111111111 111111111))
 
-(apply < (take 6666 (pal-numbers 9999999)))
 
-(= (nth (pal-numbers 0) 10101)
+(= (set (take 199 (pal-nums 0)))
+   (set (map #(first (pal-nums %)) (range 0 10000))))
+
+(= true
+   (apply < (take 6666 (pal-nums 9999999))))
+
+
+(= (nth (pal-nums 0) 10101)
    9102019)
